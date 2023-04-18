a169ed58529d9b70fe716f421f929ad5ed33f10a
james59222 <james59222@gmail.com>
2023-04-18 03:22:21 -0500

    added comments to shape.js

--- shape.js
+++ shape.js
@@ -1,3 +1,4 @@
+// Shape class with fill in color choices.
 class Shape {
     constructor() {
       this.color = "";
@@ -6,17 +7,20 @@ class Shape {
         this.color=color
     }
     }
-  class Circle extends Shape {
+// Circle parameters.  
+class Circle extends Shape {
     render() {
         return `<circle cx="200" cy="190" r="160" fill="${this.color}" />;`
         }
  
     }
-    class Square extends Shape {
+// Square parameters    
+class Square extends Shape {
         render() {
             return`<rect width="100%" height="100%" fill="${this.color}"/>`
         }
     }
+// Triangle parameters
     class Triangle extends Shape {
         render() {
             return`<polygon points="180, 18 244, 182 56,182" fill="${this.color}"/>;`
